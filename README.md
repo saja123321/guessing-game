53b0228a96deb0a48e7da3046e9f73b57324aebe
saja123321 <>
2021-07-28 19:48:06 +0000

    discrip today lab in readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # guessing-game
+
+Navigator : Saja Malkawi.
+Driver : Heba Al-Taweel.
+1.  I was Navigator on: https://github.com/hebahome/Aboutme-Gussing-Game-
+1. I was Driver on: https://github.com/saja123321/guessing-game.git
+-------
+
+1. ### `28/7` : convert the code to be clean by add functions 
--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 ////////////////// Verables
 let userName, check;
 let score = 0;
-let guessOut = false;
 let lukyNum = [55, 22, 2, 12, 10];
 let numberofguesses = 0;
 let isGuessed;
@@ -27,100 +26,40 @@ function animeQ(){
 
   check = prompt(' Do I Like anime !');
   checkingInputs(check , 'please Enter yes or no');
-
-
-  if (check.toLowerCase() === 'y' || check.toLowerCase() === 'yes') {
-    //  console.log("you type yes");
-    alert('no I didnt like animes');
-  } else if (check.toLowerCase() === 'n' || check.toLowerCase() === 'no') {
-    //  console.log("you type no");
-    alert('correct :D ');
-    score++;
-
-  } else {
-    //  console.log("Wrong answer");
-    alert('Wrong answer');
-
-  }
+  yesOrNo(check , 'no I didnt like animes' , 'correct :D ');
 }
 
 function cookingQ(){
   check = prompt(' Do I Like cooking !');
   checkingInputs(check , 'please enter Yes or No' );
-
-  if (check.toLowerCase() === 'y' || check.toLowerCase() === 'yes') {
-    //  console.log("you type yes");
-    alert('correct :D ');
-    score++;
-  } else if (check.toLowerCase() === 'n' || check.toLowerCase() === 'no') {
-    //  console.log("you type no");
-    alert('no I like cooking');
-  } else {
-    //  console.log("Wrong answer");
-    alert('Wrong answer');
-
-  }
+  yesOrNo(check , 'correct :D ' , 'no I like cooking ');
 }
 
 
 function booksQ(){
   check = prompt(' Do I Like reading Books !');
-
   checkingInputs(check , 'please Enter yes or no');
-
-
-  if (check.toLowerCase() === 'y' || check.toLowerCase() === 'yes') {
-    // console.log("you type yes");
-    alert('correct :D ');
-    score++;
-
-  } else if (check.toLowerCase() === 'n' || check.toLowerCase() === 'no') {
-    // console.log("you type no");
-    alert('no I like reading books');
-  } else {
-    //  console.log("Wrong answer");
-    alert('Wrong answer');
-
-  }
+  yesOrNo(check , 'correct :D ' , 'no I like reading books');
 }
 
 
 function moviesQ(){
   check = prompt(' Do I Like Watching movies !');
-
-
   checkingInputs(check , 'please Enter yes or no');
-
-
-  if (check.toLowerCase() === 'y' || check.toLowerCase() === 'yes') {
-    //  console.log("you type yes");
-    alert('correct :D ');
-    score++;
-
-  } else if (check.toLowerCase() === 'n' || check.toLowerCase() === 'no') {
-    //  console.log("you type no");
-    alert('no I like Watching movies');
-  } else {
-    //  console.log("Wrong answer");
-    alert('Wrong answer');
-
-  }
+  yesOrNo(check , 'correct :D ' , 'no I like Watching movies');
 }
 
 
 function guessLuckyNumbersQ(){
   let guess = prompt('Try to guess one of my lucky numbers');
-
-  while (!isGuessed && numberofguesses < 6) {
+  upperloop: while (!isGuessed && numberofguesses < 6) {
     numberofguesses++;
-
     for (let index = 0; index < lukyNum.length; index++) {
-      if (parseInt(guess) == lukyNum[index]) {
+      if (parseInt(guess) === lukyNum[index]) {
         alert('You guessed the correct number.');
-        isGuessed = true;
         alert('It took you ' + numberofguesses + ' tries to guess the correct number!');
         score++;
-        break;
+        break upperloop;
       }
       // else {
       //    console.log(guess + ' != ' + lukyNum[index])
@@ -129,12 +68,31 @@ function guessLuckyNumbersQ(){
 
     }
     if (!isGuessed && numberofguesses < 6) {
-      guess = prompt('  Your guess is lower than the number. Try again.');
+      guess = prompt('  You guess a wrong number. Try again.');
     }
 
   }
 
 }
+
+function yesOrNo(answer , yesMsg , noMsg){
+
+  if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
+    //  console.log("you type yes");
+    alert(yesMsg);
+  } else if (answer.toLowerCase() === 'n' || answer.toLowerCase() === 'no') {
+    //  console.log("you type no");
+    alert(noMsg);
+    score++;
+
+  }
+  else {
+    //  console.log("Wrong answer");
+    alert('Wrong answer');
+
+  }
+}
+
 
 function guessingGameQ(){
   let guessingNum = prompt('Let\'s Play a guessing game.. try to guess a number in my mind');
@@ -162,7 +120,8 @@ function randomNumber(max , min){
 }
 
 
-function moreInfo(){
+function moreInfo()
+{
   check = prompt(' Do you Like to know more about me !');
 
   checkingInputs(check , 'please Enter yes or no');
